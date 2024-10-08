78ccfa8ce1b694d71bc4d5b723b15dc8e0524819
Ecruz2026 <ecruz9@my.nl.edu>
2024-10-08 15:39:06 -0500

    Add files via upload

---
+++ P2a.py
@@ -0,0 +1,11 @@
+# Emmanuel Cruz
+# 10/8/24
+
+#This program modify the previous program such that only the users you and your instructor are greeted with their names.
+
+name = input("What is your name? ")
+if name == "Emmanuel" or name == "Ben":
+        print("Hello, " + name + "!",)
+else:
+    print("you are not welcome here")
+            

cecebf990c7108396278edf570f11657b067c505
Ecruz2026 <ecruz9@my.nl.edu>
2024-10-10 19:21:47 -0500

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,3 @@
+m= (int(input("Enter number of miles driven: (35) ")))
+g=(int(input("Enter number of gallons: (10) ")))
+print (str(m) + " miles per " + str(g) + " gallon have a safe and relaxing trip")
